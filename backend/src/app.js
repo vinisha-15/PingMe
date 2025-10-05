a1e6acb38f121810bfa4b0c4bddd59eb9bc7c5ef
vinisha-15 <dsouzatwinkle72@gmail.com>
2025-10-05 11:08:41 +0530

    ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import path from "path";
 
 const app=express();
 dotenv.config();
+const __dirname=path.resolve();
 
 const PORT=process.env.PORT|| 5000;
 app.listen(PORT,()=>{
@@ -12,6 +14,14 @@ app.listen(PORT,()=>{
 })
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 
 
