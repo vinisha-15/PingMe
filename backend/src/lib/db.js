5c6a6b2cb1a57a4b9db77324383e8f6afd6d4014
vinisha-15 <dsouzatwinkle72@gmail.com>
2025-10-12 19:27:47 +0530

    welcome email implemented

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
+import {ENV} from "./env.js";
 export const connectDB=async()=>{
     try {
-        const conn=await mongoose.connect(process.env.MONGO_URI);
+        const conn=await mongoose.connect(ENV.MONGO_URI);
         console.log("MongoDB connected",conn.connection.host);
     } catch (error) {
         console.error("MongoDB connection error",error);
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,15 @@
 import jwt from "jsonwebtoken";
+import {ENV} from "./env.js";
 
 export const generateToken=(userId,res)=>{
-   const token=jwt.sign({userId},process.env.JWT_SECRET,{
+   const token=jwt.sign({userId},ENV.JWT_SECRET,{
     expiresIn:"7d"
    });
    res.cookie("jwt",token,{
     httpOnly:true, //prevent XSS attacks :cross site scripting
     sameSite:"strict", //prevent CSRF attacks 
     maxAge:7*24*60*60*1000,
-    secure:process.env.NODE_ENV=="production"?true:false,
+    secure:ENV.NODE_ENV=="production"?true:false,
    });
    return token;
 }
