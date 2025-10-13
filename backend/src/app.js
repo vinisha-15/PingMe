1011da86be56a06c6194c177869124b0458cb0e4
vinisha-15 <dsouzatwinkle72@gmail.com>
2025-10-13 08:43:45 +0530

    update profile endpoint added

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,16 @@ import messageRoute from "./routes/message.route.js";
 import path from "path";
 import {connectDB} from "./lib/db.js";
 import {ENV} from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 const app=express();
-app.use(express.json());
 const __dirname=path.resolve();
 
 const PORT=ENV.PORT|| 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT} `);
     connectDB();
@@ -17,6 +21,7 @@ app.listen(PORT,()=>{
 app.use("/api/auth",authRoute);
 app.use("/api/messages",messageRoute);
 
+//make ready for deployment
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import {generateToken} from '../lib/utils.js'
 import {sendWelcomeEmail} from '../emails/emailHandler.js'
 import {ENV} from '../lib/env.js'
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup=async (req,res)=>{
     const {name,email,password}=req.body;
@@ -84,4 +85,21 @@ export const login=async(req,res)=>{
 export const logout=(_,res)=>{
     res.cookie("jwt","",{maxAge:0});
     res.status(200).json({message:"Logout successful"});
+}
+
+export const updateProfile=async(req,res)=>{
+    try{
+        const {profilePic}=req.body;
+        if(!profilePic){
+            return res.status(400).json({message:"Profile picture is required"});
+        }
+        const userId=req.user.id;
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+        res.status(200).json(updatedUser);
+     
+    }catch(error){
+        console.log("Error in update rpfile",error)
+        res.status(500).json({message:"Internal server error"})
+    }
 }
