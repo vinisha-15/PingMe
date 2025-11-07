231e0ef877251764ec54c6c06fa6795a6188ce5c
vinisha-15 <dsouzatwinkle72@gmail.com>
2025-11-07 21:48:20 +0530

    sidebar completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,6 +2,7 @@ import { create } from "zustand";
 import {axiosInstance} from "../lib/axios";
 import toast from "react-hot-toast";
 
+
 export const useAuthStore=create((set)=>({
     authUser:null,
     isCheckingAuth:true,
@@ -45,6 +46,16 @@ export const useAuthStore=create((set)=>({
             set({authUser:null})
         }finally{
             set({ isLoggingIn: false });
+        }
+    },
+    updateProfile:async(data)=>{
+        try{
+            const res=await axiosInstance.put("auth/update-profile",data)
+            set({authUser:res.data})
+            toast.success("Profile updated successfully")
+        }catch(error){
+            console.log("error in update profile",error)
+            toast.error(error.response.data.message)
         }
     }
 
