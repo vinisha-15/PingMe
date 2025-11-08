4bf044d1c5e963342798726a9870babea50851f2
vinisha-15 <dsouzatwinkle72@gmail.com>
2025-11-09 00:09:04 +0530

    chat header refactory

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,39 +1,46 @@
-import { useEffect } from "react";
+import { XIcon } from "lucide-react";
 import { useChatStore } from "../store/useChatStore";
-import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
-import NoChatsFound from "./NoChatsFound";
+import { useEffect } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 
-function ChatsList() {
-  const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } = useChatStore();
+function ChatHeader() {
+  const { selectedUser, setSelectedUser } = useChatStore();
   const { onlineUsers } = useAuthStore();
+  const isOnline = onlineUsers.includes(selectedUser._id);
 
   useEffect(() => {
-    getMyChatPartners();
-  }, [getMyChatPartners]);
+    const handleEscKey = (event) => {
+      if (event.key === "Escape") setSelectedUser(null);
+    };
 
-  if (isUsersLoading) return <UsersLoadingSkeleton />;
-  if (chats.length === 0) return <NoChatsFound />;
+    window.addEventListener("keydown", handleEscKey);
+
+    // cleanup function
+    return () => window.removeEventListener("keydown", handleEscKey);
+  }, [setSelectedUser]);
 
   return (
-    <>
-      {chats.map((chat) => (
-        <div
-          key={chat._id}
-          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
-          onClick={() => setSelectedUser(chat)}
-        >
-          <div className="flex items-center gap-3">
-            <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}>
-              <div className="size-12 rounded-full">
-                <img src={chat.profilePic || "/avatar.png"} alt={chat.name} />
-              </div>
-            </div>
-            <h4 className="text-slate-200 font-medium truncate">{chat.name}</h4>
+    <div
+      className="flex justify-between items-center bg-slate-800/50 border-b
+   border-slate-700/50 max-h-[84px] px-6 flex-1"
+    >
+      <div className="flex items-center space-x-3">
+        <div className={`avatar ${isOnline ? "online" : "offline"}`}>
+          <div className="w-12 rounded-full">
+            <img src={selectedUser.profilePic || "/avatar.png"} alt={selectedUser.fullName} />
           </div>
         </div>
-      ))}
-    </>
+
+        <div>
+          <h3 className="text-slate-200 font-medium">{selectedUser.fullName}</h3>
+          <p className="text-slate-400 text-sm">{isOnline ? "Online" : "Offline"}</p>
+        </div>
+      </div>
+
+      <button onClick={() => setSelectedUser(null)}>
+        <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      </button>
+    </div>
   );
 }
-export default ChatsList;
+export default ChatHeader;
